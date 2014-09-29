464e963a19cf0a9c82059717ada15f3ec40d9747
ShawnSantiago <santiagoshawn@gmail.com>
2014-09-29 16:40:57 -0400

    1.0

--- tasklist.js
+++ tasklist.js
@@ -1,7 +1,30 @@
-function taskadd(){
-    var task = document.getElementById('textinput').value;
-    var listItem=document.createElement("P");
+var listItem;
+
+function taskadd() {
+	taskNew = new objectTask();
+}
+
+function objectTask() { 
+	var task = document.getElementById('textinput').value; 
+    var listItem=document.createElement("li");
+    listItem.id = task;
     var itemText=document.createTextNode(task);
     listItem.appendChild(itemText);
+    var checkbox = document.createElement("input")
+    checkbox.type = "checkbox";
+    checkbox.name = task;
+    checkbox.id = "checkbox"; 
     document.getElementById('place').appendChild(listItem);
+    document.getElementById(task).insertBefore(checkbox,listItem.firstChild);
+
 }
+
+function deleteCheckBox(){
+	if (document.getElementById('checkbox').checked === true){ 
+		var itemNode = document.getElementById(checkbox.name);
+		itemNode.parentNode.removeChild(checkbox.name);
+		
+	}
+}
+
+
