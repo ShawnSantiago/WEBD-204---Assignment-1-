3c89657973540dea8fd3dbdfefbcfb98cad4f3ca
ShawnSantiago <santiagoshawn@gmail.com>
2014-10-05 19:42:17 -0400

    1.4

--- tasklist.js
+++ tasklist.js
@@ -1,24 +1,40 @@
 function taskadd() {
-	taskNew = new objectTask();
-}
-function objectTask() { 
-	var task = document.getElementById('textinput').value; 
-    var listItem=document.createElement("li");
-    listItem.id = task;
-    var itemText=document.createTextNode(task);
-    listItem.appendChild(itemText);
-    var checkbox = document.createElement("input")
-    checkbox.type = "checkbox";
-    checkbox.name = task;
-    checkbox.id = "checkbox"; 
-    document.getElementById('place').appendChild(listItem);
-    document.getElementById(task).insertBefore(checkbox,listItem.firstChild);
-}
-function deleteCheckBox(){
-	while (document.getElementById('checkbox').checked === true){ 
-		var itemNode = document.getElementById(checkbox.name);
-		itemNode.parentNode.removeChild(itemNode);		
-	}
+    taskNew = new objectTask();
 }
 
+function objectTask() {
+    var task = document.getElementById('textinput').value;
+    var listItem = document.createElement("li");
+    var checkbox = document.createElement("input")
+    checkbox.type = "checkbox";
+    listItem.appendChild(checkbox);
+    var itemText = document.createTextNode(task);
+    listItem.appendChild(itemText);
+    document.getElementById('place').appendChild(listItem);
+}
+
+function deleteCheckBox() {
+    var ul = document.getElementById('place');
+    var li = ul.children;
+    for (var i=0; i < li.length; i++) {
+        while(li[i] && li[i].children[0].checked) {
+            ul.removeChild(li[i]);
+        }
+    }
+}
+
+function importantCheckBox() {
+    var ul = document.getElementById('place');
+    var li = ul.children;
+    console.log(li.className)
+    for (var i=0; i < li.length; i++) {
+        if(li[i] && li[i].children[0].checked) {
+           li[i].className = "bold";
+        }
+        else {
+            li[i].className = ""
+        }
+    }
+
+}
 
